95a34941f4e332a5d67568779cb5047f6e318d87
349319327 <349319327@gapps.yrdsb.ca>
2023-12-12 21:46:27 -0800

    added boss

---
+++ BossOne.java
@@ -0,0 +1,45 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BossOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BossOne extends Actor
+{
+    /**
+     * Act - do whatever the BossOne wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int bossHp = 100;
+    public void act()
+    {
+        // Add your action code here.
+        hit();
+        die();
+    }
+    public void hit()
+    {
+        if(isTouching(Bullet.class))
+        {   
+            removeTouching(Bullet.class);
+            bossHp -= 2;
+            
+        }
+    }
+    
+    public void die()
+    {
+        if(bossHp <= 0)
+        {
+             World world = getWorld();
+            if (world != null) 
+            {
+                world.removeObject(this);
+            }
+        }
+    }
+    
+    
+}
